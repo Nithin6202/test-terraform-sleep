10c751d288e74132aa852445235c5a804bd66a21
Umar Ali <umarali@Umars-MacBook-Pro.local>
2023-09-20 13:18:43 +0530

    Initial commit

---
+++ main.tf
@@ -0,0 +1,12 @@
+resource "null_resource" "previous" {}
+
+resource "time_sleep" "sleep_in_seconds" {
+  depends_on = [null_resource.previous]
+
+  create_duration = var.sleep_time_in_seconds
+}
+
+# This resource will create (at least) 30 seconds after null_resource.previous
+resource "null_resource" "next" {
+  depends_on = [time_sleep.sleep_in_seconds]
+}
---
+++ variables.tf
@@ -0,0 +1,7 @@
+# Configure these variables
+
+variable "sleep_time_in_seconds" {
+  description = "Sleep time"
+  default     = "300s"
+}
+
